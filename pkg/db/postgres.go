7ce1a25a5ff66f4a440d6389cb023653fadfa163
Diony-source <erendalkiran2001@gmail.com>
2025-05-07 22:30:26 +0300

    feat(db): add getDNS support for dynamic database host resolution

--- pkg/db/postgres.go
+++ pkg/db/postgres.go
@@ -4,12 +4,22 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"mini-social-network-api/config"
 
 	_ "github.com/lib/pq"
 )
 
-func ConnectPostgres(dataSource string) *sql.DB {
-	db, err := sql.Open("postgres", dataSource)
+func getDSN(cfg *config.Config) string {
+	return fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		cfg.DBHost, cfg.DBPort, cfg.DBUser, cfg.DBPassword, cfg.DBName,
+	)
+}
+
+func ConnectPostgres(cfg *config.Config) *sql.DB {
+	dsn := getDSN(cfg)
+
+	db, err := sql.Open("postgres", dsn)
 	if err != nil {
 		log.Fatalf("❌ failed to open db: %v", err)
 	}
